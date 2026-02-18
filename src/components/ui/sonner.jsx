a536161bc2f1cc104c8c8a554b697cc92092892f
Alonsohazed <85210483+Alonsohazed@users.noreply.github.com>
2026-02-17 22:16:08 -0800

    Update sonner.jsx
    fix sonner

--- src/components/ui/sonner.jsx
+++ src/components/ui/sonner.jsx
@@ -1,28 +1,15 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, toast } from "sonner"
+import { Toaster as Sonner } from "sonner";
+import { useTheme } from "../../App";
 
-const Toaster = ({
-  ...props
-}) => {
-  const { theme = "system" } = useTheme()
+export function Toaster(props) {
+  const { theme } = useTheme();
 
   return (
     <Sonner
-      theme={theme}
-      className="toaster group"
-      toastOptions={{
-        classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
-        },
-      }}
-      {...props} />
+      theme={theme === "dark" ? "dark" : "light"}
+      richColors
+      closeButton
+      {...props}
+    />
   );
 }
-
-export { Toaster, toast }
